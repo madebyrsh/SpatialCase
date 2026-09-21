618ebcfd45b5b92e2b570bcba3e528989d4f564e
SEHWAN YOO <madebyrsh.dev@gmail.com>
2026-09-22 07:59:10 +0900

    chore: remove default RealityKit template content [기본 RealityKit 템플릿 콘텐츠 제거]

--- SpatialCase/ContentView.swift
+++ SpatialCase/ContentView.swift
@@ -6,13 +6,11 @@
 //
 
 import SwiftUI
-import RealityKit
+
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: .main)
-                .padding(.bottom, 50)
 
             Text("Hello, world!")
         }
